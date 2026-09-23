593ab605b93e93c0eb86f07315543dc04b9084ba
Amirtha-yazhini <68894384+Amirtha-yazhini@users.noreply.github.com>
2026-09-23 23:25:38 +0530

    Solve transformers-embedding

---
+++ transformer/transformers-embedding/transformers-embedding.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+def create_embedding_layer(vocab_size: int, d_model: int) -> nn.Embedding:
+    """
+    Returns an embedding layer with the requested dimensions.
+    """
+    return nn.Embedding(vocab_size,d_model)
+    pass
+
+def embed_tokens(embedding: nn.Embedding, tokens: torch.Tensor, d_model: int) -> torch.Tensor:
+    """
+    Returns scaled token embeddings.
+    """
+    return embedding(tokens) * math.sqrt(d_model)
+    pass
